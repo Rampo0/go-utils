41c97fd01a6353042a9e0d842d4d4499d139f432
rampo0 <36811859+Rampo0@users.noreply.github.com>
2020-09-16 16:22:42 +0700

    add oauth utils

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rampo0/go-utils
 
 go 1.13
 
-require golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+require (
+	github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+)
---
+++ oauth/oauth.go
@@ -0,0 +1,103 @@
+package oauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mercadolibre/golang-restclient/rest"
+	"github.com/rampo0/go-utils/rest_error"
+)
+
+const (
+	headerXPublic    = "X-Public"
+	headerXClientId  = "X-Client-Id"
+	headerXCallerId  = "X-Caller-Id"
+	paramAccessToken = "access_token"
+)
+
+var (
+	oauthRestClient = rest.RequestBuilder{
+		BaseURL: "http://172.25.0.5:8081",
+		Timeout: 500 * time.Millisecond,
+	}
+)
+
+type accessToken struct {
+	Id       string `json:"id"`
+	UserId   int64  `json:"user_id"`
+	ClientId int64  `json:"client_id"`
+}
+
+func AuthenticateRequest(request *http.Request) *rest_error.RestErr {
+	if request == nil {
+		return nil
+	}
+
+	cleanRequest(request)
+
+	accessTokenId := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessTokenId == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenId)
+	if err != nil {
+		if err.Status == http.StatusNotFound {
+			return rest_error.NewNotFoundError("not found object with given id")
+		}
+		return err
+	}
+
+	request.Header.Add(headerXClientId, fmt.Sprintf("%v", at.ClientId))
+	request.Header.Add(headerXCallerId, fmt.Sprintf("%v", at.UserId))
+
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+
+	request.Header.Del(headerXClientId)
+	request.Header.Del(headerXCallerId)
+}
+
+func getAccessToken(accessTokenId string) (*accessToken, *rest_error.RestErr) {
+
+	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenId))
+	if response == nil || response.Response == nil {
+		return nil, rest_error.NewInternalServerError("invalid restclient response when trying to get access token")
+	}
+
+	if response.StatusCode > 299 {
+		var restErr rest_error.RestErr
+		err := json.Unmarshal(response.Bytes(), &restErr)
+		if err != nil {
+			return nil, rest_error.NewInternalServerError("invalid error interface when trying to get access token")
+		}
+
+		return nil, &restErr
+	}
+
+	var at accessToken
+	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
+		return nil, rest_error.NewInternalServerError("error when trying to unmarshal access token response")
+	}
+	return &at, nil
+}
+
+func GetCallerId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	callerId, err := strconv.ParseInt(request.Header.Get(headerXCallerId), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return callerId
+}
